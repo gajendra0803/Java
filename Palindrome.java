5028a942536b1fd1914d67165a9abeb46fd7ebe0
gajendra0803 <gajendradewangan884@gmail.com>
2024-11-10 12:27:16 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -2,4 +2,33 @@ package programs;
 
 public class Palindrome {
 
+  public static void main(String[] args) {
+		System.out.print("Enter String: " );
+		Scanner sc = new Scanner(System.in);
+		String a =sc.nextLine();
+		int i ,j ;
+		i=0;
+		j =a.length()-1;
+		boolean flag =true;
+		
+		while(i<j && flag == true) {
+			if(a.charAt(i) != a.charAt(j)) {
+				flag =false;
+				break;
+			}
+			i++;
+			j--;
+		}
+		
+		if(flag) {
+			System.out.println("String is Pallindrome");
+		}
+		
+		else {
+			System.out.println("String is not Palindrome");
+		}
+		
+		sc.close();
+		}
+
 }
